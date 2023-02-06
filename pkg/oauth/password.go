bdf7131bc3a62a4df60feb991606e6ff7dc732f4
Craig O'Donnell <cbodonnell@comcast.net>
2023-02-05 22:25:47 -0500

    global client for password flow

--- pkg/oauth/password.go
+++ pkg/oauth/password.go
@@ -13,15 +13,6 @@ func Password(ctx context.Context) (*oauth2.Token, error) {
 	username := term.StringPrompt("Username:")
 	password := term.PasswordPrompt("Password:")
 
-	conf := &oauth2.Config{
-		ClientID:     ClientId,
-		ClientSecret: "",
-		Endpoint: oauth2.Endpoint{
-			TokenURL: fmt.Sprintf("%s/realms/%s/protocol/openid-connect/token", KeycloakServerUrl, RealmName),
-		},
-		Scopes: []string{"openid", "profile", "email"},
-	}
-
 	token, err := conf.PasswordCredentialsToken(ctx, username, password)
 	if err != nil {
 		log.Fatal(err)
